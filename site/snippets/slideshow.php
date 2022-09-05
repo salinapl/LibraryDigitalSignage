ac8f4ca70a62c8cc3e88bf1b20011a6db4b6eb96
Swizzler <git@samswitzer.com>
2022-09-04 22:54:30 -0500

    create error slide

--- site/snippets/slideshow.php
+++ site/snippets/slideshow.php
@@ -1,7 +1,11 @@
 <?php 
     $url = $_SERVER['REQUEST_URI'];
+    // Fetches the selected gallery based on the campaign page
+    // then filters the gallery based on the selected tags.
     $gallery = page($page->gallery())->images();
-    $gallery = $gallery->filterBy('tags', 'in', $page->tags()->split(','), ',');
+    $gallery = $gallery->filterBy(
+        'tags', 'in', $page->tags()->split(','), ','
+    );
 
     // orientation can be passed by snippet if the inclusion
     // Is embeded into the page (see opac page for example)
@@ -11,11 +15,26 @@
     } elseif(strpos($url, 'landscape')){
         $orientation = 'landscape';
     }
+
+    // Creates an empty array then filters gallery first on if an image is
+    // within it's active date range, then on its orientation. Afterward it
+    // assembles the html and outputs the result into the array.
     $slides = array();
     $class = 'class="carousel-cell ad" style="background-image:url(';
     foreach ($gallery->sortBy('sort','asc') as $image){
         $string = "<a ";
-        if (($image->expire()->toDate('Y-m-d') > date('Y-m-d')) && ($image->start()->toDate('Y-m-d') <= date('Y-m-d'))) {
+        if (
+            (
+                $image
+                    ->expire()
+                    ->toDate('Y-m-d') > date('Y-m-d')
+            ) && 
+            (
+                $image
+                    ->start()
+                    ->toDate('Y-m-d') <= date('Y-m-d')
+            )
+        ) {
             if($image->orientation() == $orientation){
                 if($image->orientation() == 'portrait'){
                     if ($image->link()->isNotEmpty()){
@@ -35,21 +54,40 @@
             }
         }
     }
-    // Script for Extracting Web Slides
+    // Queries the web-slides page and gets an array of it's child pages,
+    // it then filters the pages based on the selected tags. The error page
+    // will never be selected as it does not have any set tags.
     $webslides = page('web-slides')->children();
-    $webslides = $webslides->filterBy('tags', 'in', $page->tags()->split(','), ',');
+    $webslides = $webslides->filterBy(
+        'tags', 'in', $page->tags()->split(','), ','
+    );
 
     foreach ($webslides->sortBy('sort','asc') as $slide){
         $string = '<iframe class="carousel-cell" src="';
-        if (($slide->expire()->toDate('Y-m-d') > date('Y-m-d')) && ($slide->start()->toDate('Y-m-d') <= date('Y-m-d'))){
+        if (
+            (
+                $slide
+                    ->expire()
+                    ->toDate('Y-m-d') > date('Y-m-d')
+            ) && (
+                $slide
+                    ->start()
+                    ->toDate('Y-m-d') <= date('Y-m-d')
+            )
+        ){
             $string .= $slide->url();
             $string .= '" scrolling="no"></iframe>';
             array_push($slides, $string);
         }
     }
-    if (count($slides) > 0){
-
+    // Counts the number of slides in the array, if it's zero, throws error slide 
+    if (count($slides) <= 0){
+        $string = '<iframe class="carousel-cell" src="';
+        $string .= $site->page('web-slides/error-slide')->url();
+        $string .= '" scrolling="no"></iframe>';
+        array_push($slides, $string);
     }
+    // Sorts the sides (random) and then prints each slide into html
     shuffle($slides);
     foreach($slides as $slide){
         echo $slide . "\n";
--- site/templates/error-slide.php
+++ site/templates/error-slide.php
@@ -1,4 +1,12 @@
 <?php snippet('header') ?>
-        <?= css('assets/css/templates/error.css') ?>
+        <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+        <?= css('assets/css/templates/error-slide.css') ?>
     </head>
     <body>
+        <div class="flex-box">
+            <div class="flex-item">
+                <h1><i class="<?= $page->icon() ?>"></i><?= $page->headline() ?></h1>
+                <?= $page->body()->kirbytext() ?>
+            </div>
+        </div>
+        <?php snippet('footer') ?>
