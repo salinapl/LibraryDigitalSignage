59a96a206d3de443dfca4be1e9c73d06b9d9cf21
Swizzler121 <36831696+Swizzler121@users.noreply.github.com>
2023-03-24 17:51:24 -0500

    builder.php refactor
    refactor of builder.php in an attempt to fix filtering bug

--- site/snippets/builder.php
+++ site/snippets/builder.php
@@ -1,11 +1,5 @@
 <?php 
     $url = $_SERVER['REQUEST_URI'];
-    // Fetches the selected gallery based on the campaign page
-    // then filters the gallery based on the selected tags.
-    $gallery = page($page->gallery())->images();
-    $gallery = $gallery->filterBy(
-        'tags', 'in', $page->tags()->split(','), ','
-    );
 
     // orientation can be passed by snippet if the inclusion
     // Is embeded into the page (see opac page for example)
@@ -16,70 +10,76 @@
         $orientation = 'landscape';
     }
 
-    // Creates an empty array then filters gallery first on if an image is
-    // within it's active date range, then on its orientation. Afterward it
-    // assembles the html and outputs the result into the array.
-    $slides = array();
-    $class = 'class="carousel-cell ad" style="background-image:url(';
-    foreach ($gallery->sortBy('sort','asc') as $image){
-        $string = "<a ";
-        if (
-            (
-                $image
-                    ->expire()
-                    ->toDate('Y-m-d') > date('Y-m-d')
-            ) && 
-            (
-                $image
-                    ->start()
-                    ->toDate('Y-m-d') <= date('Y-m-d')
-            )
-        ) {
-            if($image->orientation() == $orientation){
-                if($image->orientation() == 'portrait'){
-                    if ($image->link()->isNotEmpty()){
-                        $string .= "href={$image->link()->url()} ";
-                    }
-                    $string .= $class;
-                    $string .= $image->resize(null, 1080)->url();
-                    $string .= ')"></a>';
-                    array_push($slides, $string);
-                }
-                else {
-                    $string .= $class;
-                    $string .= $image->resize(1080, null)->url();
-                    $string .= ')"></a>';
-                    array_push($slides, $string);
-                }
-            }
-        }
-    }
+    // Fetches the selected gallery based on the campaign page
+    // then filters the gallery based on the selected tags.
+    $gallery = page($page->gallery())
+                    ->images()
+                    ->filterBy('tags', 'in', $page->tags()->split(','), ',');
+    
+    // Filters images further by orientation and resizes the images
+    $gallery = $gallery->filter(function ($image) use ($orientation) {
+        return $image->orientation() == $orientation;
+    });
+
+    // Filters the images to only show ones that appear between
+    // the campaigns start and end date
+    $gallery = $gallery->filter(function ($image) {
+        return 
+            $image
+                ->expire()
+                ->toDate('Y-m-d') > date('Y-m-d')
+            &&
+            $image
+                ->start()
+                ->toDate('Y-m-d') <= date('Y-m-d');
+    });
+
     // Queries the web-slides page and gets an array of it's child pages,
     // it then filters the pages based on the selected tags. The error page
     // will never be selected as it does not have any set tags.
-    $webslides = page('web-slides')->children();
-    $webslides = $webslides->filterBy(
-        'tags', 'in', $page->tags()->split(','), ','
-    );
+    $webslides = page('web-slides')
+                    ->children()
+                    ->listed()
+                    ->filterBy('tags', 'in', $page->tags()->split(','), ',');
 
-    foreach ($webslides->sortBy('sort','asc') as $slide){
-        $string = '<iframe class="carousel-cell" src="';
-        if (
-            (
-                $slide
-                    ->expire()
-                    ->toDate('Y-m-d') > date('Y-m-d')
-            ) && (
-                $slide
-                    ->start()
-                    ->toDate('Y-m-d') <= date('Y-m-d')
-            )
-        ){
-            $string .= $slide->url();
-            $string .= '" scrolling="no"></iframe>';
-            array_push($slides, $string);
+    $webslides = $webslides->filter(function ($webslide) {
+        return
+            $webslide
+                ->expire()
+                ->toDate('Y-m-d') > date('Y-m-d')
+            &&
+            $webslide
+                ->start()
+                ->toDate('Y-m-d') <= date('Y-m-d');
+    });
+
+    // Creates an empty array then assembles the slides into strings
+    // then assembles the html and outputs the result into the array.
+    $slides = array();
+    $class = 'class="carousel-cell ad" style="background-image:url(';
+    foreach ($gallery as $image){
+        $string = "<a ";
+        if ($image->link()->isNotEmpty()){
+            $string .= "href={$image->link()->url()} ";
         }
+        $string .= $class;
+        if($image->orientation() == 'portrait'){
+            $string .= $image->resize(null, 1080)->url();
+        }
+        else { 
+            $string .= $image->resize(1080, null)->url();
+        }
+        $string .= ')"></a>';
+        array_push($slides, $string);
     }
+
+    foreach ($webslides as $webslide){
+        $string = '<iframe class="carousel-cell" src="';
+        $string .= $webslide->url();
+        $string .= '" scrolling="no"></iframe>';
+        array_push($slides, $string);
+    }    
+
     // Counts the number of slides in the array, if it's zero, throws error slide 
     if (count($slides) <= 0){
         $string = '<iframe class="carousel-cell" src="';
@@ -92,6 +92,5 @@
     foreach($slides as $slide){
         echo $slide . "\n";
     }
-
-
+    
 ?>
